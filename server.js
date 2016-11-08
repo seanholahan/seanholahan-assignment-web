778c25ea25b256b1240588079672fa34f5c44e40
seanholahan <holahan.s@husky.neu.edu>
2016-11-07 19:39:22 -0500

    adding server.js, package and readme

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// configure a public directory to host static content
+app.use(express.static(__dirname + '/public'));
+
+require ("./test/app.js")(app);
+
+require("./assignment/app.js")(app);
+
+var websites = [
+    {_id: 321, name: 'facebook.com', uid: 123},
+    {_id: 432, name: 'wikipedia.org', uid: 123},
+    {_id: 543, name: 'twitter.com', uid: 234}
+];
+
+app.get("/websites", function(req, res){
+    res.send(websites);
+});
+
+
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 63342;
+
+app.listen(port, ipaddress);;
